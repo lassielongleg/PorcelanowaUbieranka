05cad83415991fe3f6d0b57b826f965a2df5abca
lassielongleg <s30594@pjwstk.edu.pl>
2026-04-14 15:56:35 +0200

    audio button

--- Assets/scripts/MuteAudio.cs
+++ Assets/scripts/MuteAudio.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class MuteAudio : MonoBehaviour
 {
-    
+    public void MuteToggle(bool muted)
+    {
+        if (muted)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+        }
+    }
 }
