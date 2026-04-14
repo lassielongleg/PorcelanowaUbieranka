1716d80be6c3811d131593fc6cdbecdb647a89a0
lassielongleg <s30594@pjwstk.edu.pl>
2026-04-14 16:09:59 +0200

    whatever

--- Assets/scripts/ChangeScene.cs
+++ Assets/scripts/ChangeScene.cs
@@ -2,23 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeScene : MonoBehaviour
+public class ChangeBackground : MonoBehaviour
 {
-    public GameObject tlodom;
-    public GameObject tloogrod;
-    
+    public GameObject[] backgrounds;
+    private int backgroundNum = 0;
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+    // Update is called once per frame
     void Update()
     {
         
     }
 
-    public void BackgroundChanger()
+    public void BackGroundChangerLeft()
     {
-            tlodom.SetActive(false);
-            tloogrod.SetActive(true);
+        backgrounds[backgroundNum].SetActive(false);
+        backgroundNum--;
+        if (backgroundNum < 0)
+        {
+            backgroundNum = backgrounds.Length-1;
+        }
+        backgrounds[backgroundNum].SetActive(true);
+
+    }
+
+    public void BackGroundChangerRight()
+    {
+        backgrounds[backgroundNum].SetActive(false);
+        backgroundNum++;
+        if (backgroundNum == backgrounds.Length)
+        {
+            backgroundNum = 0;
+        }
+        backgrounds[backgroundNum].SetActive(true);
     }
 }
